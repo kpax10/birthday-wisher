0091b9d75cf949204997d2931f6998a541741a8d
Kyle <notxap55@yahoo.com>
2024-10-09 16:26:08 -0700

    datetime started

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-import smtplib
+# import smtplib
+#
+# my_email = "timmyt345345@gmail.com"
+# password = "qbtnsxioriyqxijz"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="anniet345345@yahoo.com",
+#         msg="Subject:Hello\n\nThis is the body of my email."
+#     )
 
-my_email = "timmyt345345@gmail.com"
-password = "qbtnsxioriyqxijz"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="anniet345345@yahoo.com",
-        msg="Subject:Hello\n\nThis is the body of my email."
-    )
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+print(year)
+
